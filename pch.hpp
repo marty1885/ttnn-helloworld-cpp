6245f4d619f0350109b9067a3ea658fa373db599
marty1885 <marty188586@gmail.com>
2024-06-25 08:52:50 +0000

    update PCH

--- pch.hpp
+++ pch.hpp
@@ -1,1 +1,11 @@
-#include "tt_metal/host_api.hpp"
+#include "tensor/host_buffer/functions.hpp"
+#include "tensor/types.hpp"
+#include "tt_dnn/op_library/auto_format.hpp"
+#include <cstddef>
+#include <tt_eager/tensor/tensor.hpp>
+#include <ttnn/core.hpp>
+#include <ttnn/operations/eltwise/binary/binary.hpp>
+#include <ttnn/device.hpp>
+#include <tt_dnn/op_library/fully_connected/fully_connected_op.hpp>
+#include <tt_dnn/op_library/eltwise_unary/eltwise_unary_op.hpp>
+#include <tt_eager/tensor/tensor.hpp>
